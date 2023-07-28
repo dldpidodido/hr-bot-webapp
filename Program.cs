cdcf5373fe924139e0106e3844daa1d9d29abd0c
Melfred <melfredmacabenta.hplgamedesign@gmail.com>
2023-07-28 17:22:17 +0800

    * Update to Blazor Modal

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Blazored.Modal;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazoredModal();
+
 
 var app = builder.Build();
 
